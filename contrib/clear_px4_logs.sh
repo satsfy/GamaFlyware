998619f0696c2874d5a9040cddada0b92d4098cd
RenatoBrittoAraujo <renatobritto.professional@gmail.com>
2026-03-27 20:14:46 +0000

    feat(contrib): add helper scripts

---
+++ contrib/clear_px4_logs.sh
@@ -0,0 +1,2 @@
+rm -rf /sitl/PX4-Autopilot/build/px4_sitl_default/rootfs/log/*
+rm /home/qgcuser/Documents/QGroundControl/Telemetry/*
---
+++ contrib/kill_matplot_instances.sh
@@ -0,0 +1,12 @@
+sudo apt-get install xdotool
+
+
+xdotool search --name 'Figure' | xargs -r xdotool windowclose
+xdotool search --name 'Figure' | xargs -r xdotool windowclose
+xdotool search --name 'Figure' | xargs -r xdotool windowclose
+xdotool search --name 'Figure' | xargs -r xdotool windowclose
+xdotool search --name 'Figure' | xargs -r xdotool windowclose
+xdotool search --name 'Figure' | xargs -r xdotool windowclose
+
+
+
